2461010fbe57f29f1583032b24b7bb7ebf8ad692
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-03-20 16:55:07 +0530

    made project structure

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+require("./app/middleware/db");
+
+const bodyParse = require("body-parser");
+
+require("dotenv").config();
+app.use(express.json());
+app.use(bodyParse.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
