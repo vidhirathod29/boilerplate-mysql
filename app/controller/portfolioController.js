329b84dee7094845624248fdd33c3eb6559077b0
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-03-29 18:57:51 +0530

    changes: applied common try & catch block and change the response format

--- app/controller/portfolioController.js
+++ app/controller/portfolioController.js
@@ -1,68 +1,55 @@
 const connection = require('../middleware/db');
-const logger = require('../logger/logger');
 const { GeneralError } = require('../utils/error');
 const StatusCodes = require('../utils/Http-Status');
 const { Messages } = require('../utils/messages');
 const { RESPONSE_STATUS } = require('../utils/enum');
 
 const addPortfolio = async (req, res, next) => {
-  try {
-    const {
-      projectCategory,
-      projectName,
-      projectTitle,
-      projectDate,
-      description,
-      categoryId,
-    } = req.body;
+  const {
+    projectCategory,
+    projectName,
+    projectTitle,
+    projectDate,
+    description,
+    categoryId,
+  } = req.body;
 
-    if (req.files) {
-      const images = req.files.map((images) => images.filename).join(',');
-      connection.query(
-        `INSERT INTO portfolio
+  if (req.files) {
+    const images = req.files.map((images) => images.filename).join(',');
+    connection.query(
+      `INSERT INTO portfolio
             (categoryId, projectCategory, projectName, projectTitle, projectDate, description,images) 
             VALUES ('${categoryId}','${projectCategory}','${projectName}','${projectTitle}','${projectDate}','${description}','${images}')`,
-        (err, results) => {
-          if (err) {
-            next(
-              new GeneralError(
-                Messages.SOMETHING_WENT_WRONG,
-                StatusCodes.NOT_FOUND,
-                undefined,
-                RESPONSE_STATUS.ERROR,
-              ),
-            );
-          }
+      (err, results) => {
+        if (err) {
+          next(
+            new GeneralError(
+              Messages.SOMETHING_WENT_WRONG,
+              StatusCodes.NOT_FOUND,
+              undefined,
+              RESPONSE_STATUS.ERROR,
+            ),
+          );
+        }
 
-          if (results) {
-            next(
-              new GeneralError(
-                `Portfolio ${Messages.ADD_SUCCESS}`,
-                StatusCodes.OK,
-                undefined,
-                RESPONSE_STATUS.SUCCESS,
-              ),
-            );
-          }
-        },
-      );
-    }
-  } catch (error) {
-    next(
-      new GeneralError(
-        Messages.SERVER_ERROR,
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        undefined,
-        RESPONSE_STATUS.ERROR,
-      ),
+        if (results) {
+          next(
+            new GeneralError(
+              `Portfolio ${Messages.ADD_SUCCESS}`,
+              StatusCodes.OK,
+              undefined,
+              RESPONSE_STATUS.SUCCESS,
+            ),
+          );
+        }
+      },
     );
   }
 };
 
 const viewPortfolio = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const sql = `
+  const id = req.params.id;
+  const sql = `
       SELECT 
         portfolio.*, 
         category.id AS categoryId,
@@ -83,183 +70,8 @@ const viewPortfolio = async (req, res, next) => {
         portfolio.id = ${id};
     `;
 
-    connection.query(sql, (err, result) => {
-      if (err) {
-        next(
-          new GeneralError(
-            Messages.SOMETHING_WENT_WRONG,
-            StatusCodes.BAD_REQUEST,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-      if (result.length === 0) {
-        next(
-          new GeneralError(
-            `Portfolio ${Messages.NOT_FOUND}`,
-            StatusCodes.NOT_FOUND,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      } else {
-        next(
-          new GeneralError(
-            Messages.GET_SUCCESS,
-            StatusCodes.OK,
-            result[0],
-            RESPONSE_STATUS.SUCCESS,
-          ),
-        );
-      }
-    });
-  } catch (error) {
-    next(
-      new GeneralError(
-        Messages.SERVER_ERROR,
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        undefined,
-        RESPONSE_STATUS.ERROR,
-      ),
-    );
-  }
-};
-
-const updatePortfolio = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const sql = `SELECT * FROM portfolio WHERE id=${id}`;
-    connection.query(sql, (err, result) => {
-      if (err) {
-        next(
-          new GeneralError(
-            Messages.SOMETHING_WENT_WRONG,
-            StatusCodes.BAD_REQUEST,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-      if (result.length > 0) {
-        const setClause = Object.entries(req.body)
-          .map(([key, value]) => `${key}='${value}'`)
-          .join(', ');
-
-        const sql = `UPDATE portfolio SET ${setClause}  WHERE id=${id}`;
-        connection.query(sql, (err, result) => {
-          if (err) {
-            next(
-              new GeneralError(
-                Messages.SOMETHING_WENT_WRONG,
-                StatusCodes.BAD_REQUEST,
-                undefined,
-                RESPONSE_STATUS.ERROR,
-              ),
-            );
-          }
-          if (result) {
-            next(
-              new GeneralError(
-                `Portfolio ${Messages.UPDATED_SUCCESS}`,
-                StatusCodes.OK,
-                undefined,
-                RESPONSE_STATUS.SUCCESS,
-              ),
-            );
-          }
-        });
-      } else {
-        next(
-          new GeneralError(
-            Messages.USER_NOT_FOUND,
-            StatusCodes.NOT_FOUND,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-    });
-  } catch (error) {
-    next(
-      new GeneralError(
-        Messages.SERVER_ERROR,
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        undefined,
-        RESPONSE_STATUS.ERROR,
-      ),
-    );
-  }
-};
-
-const deletePortfolio = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const sql = `SELECT * FROM portfolio WHERE id=${id}`;
-    connection.query(sql, (err, result) => {
-      if (err) {
-        next(
-          new GeneralError(
-            Messages.SOMETHING_WENT_WRONG,
-            StatusCodes.BAD_REQUEST,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-      if (result.length > 0) {
-        const sql = `DELETE FROM portfolio WHERE id =${id}`;
-        connection.query(sql, (err, result) => {
-          if (err) {
-            next(
-              new GeneralError(
-                Messages.SOMETHING_WENT_WRONG,
-                StatusCodes.BAD_REQUEST,
-                undefined,
-                RESPONSE_STATUS.ERROR,
-              ),
-            );
-          }
-          if (result) {
-            next(
-              new GeneralError(
-                Messages.DELETE_SUCCESS,
-                StatusCodes.OK,
-                undefined,
-                RESPONSE_STATUS.SUCCESS,
-              ),
-            );
-          }
-        });
-      } else {
-        next(
-          new GeneralError(
-            Messages.NOT_FOUND,
-            StatusCodes.NOT_FOUND,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-    });
-  } catch (error) {
-    logger.error('Error', error);
-    next(
-      new GeneralError(
-        Messages.SERVER_ERROR,
-        StatusCodes.INTERNAL_SERVER_ERROR,
-        undefined,
-        RESPONSE_STATUS.ERROR,
-      ),
-    );
-  }
-};
-
-const deleteMultiplePortfolio = async (req, res, next) => {
-  try {
-    const { ids } = req.body;
-
-    if (!Array.isArray(ids) || ids.length === 0) {
+  connection.query(sql, (err, result) => {
+    if (err) {
       next(
         new GeneralError(
           Messages.SOMETHING_WENT_WRONG,
@@ -269,51 +81,181 @@ const deleteMultiplePortfolio = async (req, res, next) => {
         ),
       );
     }
-    const sql = `DELETE FROM portfolio WHERE id IN(${ids.join(',')})`;
+    if (result.length === 0) {
+      next(
+        new GeneralError(
+          `Portfolio ${Messages.NOT_FOUND}`,
+          StatusCodes.NOT_FOUND,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    } else {
+      next(
+        new GeneralError(
+          Messages.GET_SUCCESS,
+          StatusCodes.OK,
+          result[0],
+          RESPONSE_STATUS.SUCCESS,
+        ),
+      );
+    }
+  });
+};
 
-    connection.query(sql, (err, result) => {
-      if (err) {
-        next(
-          new GeneralError(
-            Messages.SOMETHING_WENT_WRONG,
-            StatusCodes.BAD_REQUEST,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-        return;
-      }
-      if (result.affectedRows > 0) {
-        next(
-          new GeneralError(
-            Messages.DELETE_SUCCESS,
-            StatusCodes.OK,
-            undefined,
-            RESPONSE_STATUS.SUCCESS,
-          ),
-        );
-      } else {
-        next(
-          new GeneralError(
-            Messages.NOT_FOUND,
-            StatusCodes.NOT_FOUND,
-            undefined,
-            RESPONSE_STATUS.ERROR,
-          ),
-        );
-      }
-    });
-  } catch (error) {
-    logger.error('Error', error);
+const updatePortfolio = async (req, res, next) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM portfolio WHERE id=${id}`;
+  connection.query(sql, (err, result) => {
+    if (err) {
+      next(
+        new GeneralError(
+          Messages.SOMETHING_WENT_WRONG,
+          StatusCodes.BAD_REQUEST,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    }
+    if (result.length > 0) {
+      const setClause = Object.entries(req.body)
+        .map(([key, value]) => `${key}='${value}'`)
+        .join(', ');
+
+      const sql = `UPDATE portfolio SET ${setClause}  WHERE id=${id}`;
+      connection.query(sql, (err, result) => {
+        if (err) {
+          next(
+            new GeneralError(
+              Messages.SOMETHING_WENT_WRONG,
+              StatusCodes.BAD_REQUEST,
+              undefined,
+              RESPONSE_STATUS.ERROR,
+            ),
+          );
+        }
+        if (result) {
+          next(
+            new GeneralError(
+              `Portfolio ${Messages.UPDATED_SUCCESS}`,
+              StatusCodes.OK,
+              undefined,
+              RESPONSE_STATUS.SUCCESS,
+            ),
+          );
+        }
+      });
+    } else {
+      next(
+        new GeneralError(
+          Messages.USER_NOT_FOUND,
+          StatusCodes.NOT_FOUND,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    }
+  });
+};
+
+const deletePortfolio = async (req, res, next) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM portfolio WHERE id=${id}`;
+  connection.query(sql, (err, result) => {
+    if (err) {
+      next(
+        new GeneralError(
+          Messages.SOMETHING_WENT_WRONG,
+          StatusCodes.BAD_REQUEST,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    }
+    if (result.length > 0) {
+      const sql = `DELETE FROM portfolio WHERE id =${id}`;
+      connection.query(sql, (err, result) => {
+        if (err) {
+          next(
+            new GeneralError(
+              Messages.SOMETHING_WENT_WRONG,
+              StatusCodes.BAD_REQUEST,
+              undefined,
+              RESPONSE_STATUS.ERROR,
+            ),
+          );
+        }
+        if (result) {
+          next(
+            new GeneralError(
+              Messages.DELETE_SUCCESS,
+              StatusCodes.OK,
+              undefined,
+              RESPONSE_STATUS.SUCCESS,
+            ),
+          );
+        }
+      });
+    } else {
+      next(
+        new GeneralError(
+          Messages.NOT_FOUND,
+          StatusCodes.NOT_FOUND,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    }
+  });
+};
+
+const deleteMultiplePortfolio = async (req, res, next) => {
+  const { ids } = req.body;
+
+  if (!Array.isArray(ids) || ids.length === 0) {
     next(
       new GeneralError(
-        Messages.SERVER_ERROR,
-        StatusCodes.INTERNAL_SERVER_ERROR,
+        Messages.SOMETHING_WENT_WRONG,
+        StatusCodes.BAD_REQUEST,
         undefined,
         RESPONSE_STATUS.ERROR,
       ),
     );
   }
+  const sql = `DELETE FROM portfolio WHERE id IN(${ids.join(',')})`;
+
+  connection.query(sql, (err, result) => {
+    if (err) {
+      next(
+        new GeneralError(
+          Messages.SOMETHING_WENT_WRONG,
+          StatusCodes.BAD_REQUEST,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+      return;
+    }
+    if (result.affectedRows > 0) {
+      next(
+        new GeneralError(
+          Messages.DELETE_SUCCESS,
+          StatusCodes.OK,
+          undefined,
+          RESPONSE_STATUS.SUCCESS,
+        ),
+      );
+    } else {
+      next(
+        new GeneralError(
+          Messages.NOT_FOUND,
+          StatusCodes.NOT_FOUND,
+          undefined,
+          RESPONSE_STATUS.ERROR,
+        ),
+      );
+    }
+  });
 };
 
 module.exports = {
--- app/routes/portfolioRoute.js
+++ app/routes/portfolioRoute.js
@@ -5,12 +5,36 @@ const validate = require('../validation/portfolioValidation');
 const { validator } = require('../middleware/validator');
 const upload = require('../middleware/multer');
 const { authentication } = require('../middleware/authenticate');
+const { errorHandler } = require('../helper/error');
 
-router.post('/addPortfolio',authentication,upload.array('images',5),validator.body(validate.portfolio),controller.addPortfolio);
-router.get('/viewPortfolio/:id',authentication,controller.viewPortfolio);
-router.put('/updatePortfolio/:id',authentication,upload.array('images',5),validator.body(validate.updatePortfolio),controller.updatePortfolio);
-router.delete('/deletePortfolio/:id',authentication,controller.deletePortfolio);
-router.delete('/deleteMultiplePortfolio',authentication,controller.deleteMultiplePortfolio);
+router.post(
+  '/addPortfolio',
+  authentication,
+  upload.array('images', 5),
+  validator.body(validate.portfolio),
+  errorHandler(controller.addPortfolio),
+);
+router.get(
+  '/viewPortfolio/:id',
+  authentication,
+  errorHandler(controller.viewPortfolio),
+);
+router.put(
+  '/updatePortfolio/:id',
+  authentication,
+  upload.array('images', 5),
+  validator.body(validate.updatePortfolio),
+  errorHandler(controller.updatePortfolio),
+);
+router.delete(
+  '/deletePortfolio/:id',
+  authentication,
+  errorHandler(controller.deletePortfolio),
+);
+router.delete(
+  '/deleteMultiplePortfolio',
+  authentication,
+  errorHandler(controller.deleteMultiplePortfolio),
+);
 
-
-module.exports=router;
+module.exports = router;
